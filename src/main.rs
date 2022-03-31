beec0e8f0f93ba04d45838191d0fb145b607bc7d
amunds1 <andreas.amundsen123@gmail.com>
2022-03-31 20:20:22 +0200

    Format files

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,88 @@
-use chrono::offset::Utc;
-use chrono::{Datelike, DateTime, Duration, FixedOffset};
+use std::fs::File;
+use std::io::prelude::*;
 
+use chrono::{Datelike, DateTime, Duration, FixedOffset};
 use chrono::NaiveDateTime;
+use chrono::offset::Utc;
 use icalendar::Calendar;
 use icalendar::Component;
 use icalendar::Event;
 use serde::Deserialize;
-use std::fs::File;
-use std::io::prelude::*;
 
 #[derive(Deserialize, Debug)]
 struct CircuitType {
-	circuitId: String,
-	url: String,
-	circuitName: String,
-	Location: LocationType,
+    circuitId: String,
+    url: String,
+    circuitName: String,
+    Location: LocationType,
 }
 
 #[derive(Deserialize, Debug)]
 struct LocationType {
-	lat: String,
-	long: String,
-	locality: String,
-	country: String,
+    lat: String,
+    long: String,
+    locality: String,
+    country: String,
 }
 
 #[derive(Deserialize, Debug)]
 struct RaceType {
-	season: String,
-	round: String,
-	url: String,
-	raceName: String,
-	Circuit: CircuitType,
-	date: String,
-	time: String,
+    season: String,
+    round: String,
+    url: String,
+    raceName: String,
+    Circuit: CircuitType,
+    date: String,
+    time: String,
 }
 
 #[derive(Deserialize, Debug)]
 struct RaceListType {
-	Races: Vec<RaceType>,
+    Races: Vec<RaceType>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// Fetch race data
-	let data = reqwest::get(format!("http://ergast.com/api/f1/{}.json", Utc::now().year()))
-		.await?
-		.json::<serde_json::Value>()
-		.await?;
+    // Fetch race data
+    let data = reqwest::get(format!("http://ergast.com/api/f1/{}.json", Utc::now().year()))
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
 
-	// Store races in JSON data as RaceListType
-	let races: RaceListType = serde_json::from_value(data["MRData"]["RaceTable"].clone()).unwrap();
+    // Store races in JSON data as RaceListType
+    let races: RaceListType = serde_json::from_value(data["MRData"]["RaceTable"].clone()).unwrap();
 
-	// Create a empty calendar
-	let mut raceCalendar = Calendar::new();
+    // Create a empty calendar
+    let mut raceCalendar = Calendar::new();
 
-	// Iterate over races
-	for race in races.Races {
-		// Convert race date and time into DateTime<Utc>
-		let ndt = NaiveDateTime::parse_from_str(format!("{} {}", race.date, race.time).as_str(), "%Y-%m-%d %H:%M:%S%Z");
-		let dt = DateTime::<Utc>::from_utc(ndt.unwrap(), Utc);
+    // Iterate over races
+    for race in races.Races {
+        // Convert race date and time into DateTime<Utc>
+        let ndt = NaiveDateTime::parse_from_str(format!("{} {}", race.date, race.time).as_str(), "%Y-%m-%d %H:%M:%S%Z");
+        let dt = DateTime::<Utc>::from_utc(ndt.unwrap(), Utc);
 
-		// Create race event
-		let event = Event::new()
-			.starts(dt)
-			.ends(dt + Duration::hours(2))
-			.summary(&race.raceName.to_string())
-			.location(
-				&format!(
-					"{}, {}",
-					race.Circuit.Location.locality, race.Circuit.Location.country
-				)
-				.to_string(),
-			)
-			.done();
+        // Create race event
+        let event = Event::new()
+            .starts(dt)
+            .ends(dt + Duration::hours(2))
+            .summary(&race.raceName.to_string())
+            .location(
+                &format!(
+                    "{}, {}",
+                    race.Circuit.Location.locality, race.Circuit.Location.country
+                )
+                    .to_string(),
+            )
+            .done();
 
-		// Push event to calendar
-		raceCalendar.push(event);
-	}
+        // Push event to calendar
+        raceCalendar.push(event);
+    }
 
-	// Write calendar to a .ics file
-	let data = raceCalendar.to_string();
-	let mut f = File::create("f1-races.ics").expect("Unable to create file");
-	f.write_all(data.as_bytes()).expect("Unable to write data");
+    // Write calendar to a .ics file
+    let data = raceCalendar.to_string();
+    let mut f = File::create("f1-races.ics").expect("Unable to create file");
+    f.write_all(data.as_bytes()).expect("Unable to write data");
 
-	Ok(())
+    Ok(())
 }
